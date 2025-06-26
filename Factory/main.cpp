3030340d0089ae6f9ff13ae8dc1898677ce2900e
emiliaMetelitsa <klthsrswm@gmail.com>
2025-06-26 12:24:36 +0300

    Create main.cpp

---
+++ Factory/main.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include "classes.hpp"
+
+int main() {
+	std::vector <IPrintable*> v;
+
+	Factory* animal = new CreateAnimal;
+	Factory* fish = new CreateFish;
+	Factory* bird = new CreateBird;
+
+	v.emplace_back(animal->Create());
+	v.emplace_back(animal->Create());
+	v.emplace_back(fish->Create());
+	v.emplace_back(fish->Create());
+	v.emplace_back(fish->Create());
+	v.emplace_back(bird->Create());
+	v.emplace_back(bird->Create());
+	v.emplace_back(bird->Create());
+	v.emplace_back(bird->Create());
+
+	delete[] animal;
+	delete[] fish;
+	delete[] bird;
+
+	for (int i = 0; i < size(v); i++) {
+		v[i]->Print();
+	}
+
+	v.erase(v.begin(), v.end());
+
+
+}
