0ebd30b246a4848be6f28a895299e6c2418f46ee
emiliaMetelitsa <klthsrswm@gmail.com>
2025-06-27 13:42:24 +0300

    Create main.cpp

---
+++ SmartPointers/ClassesFabric/main.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include "classes.hpp"
+#include <memory>
+
+int main() {
+	using namespace std;
+
+	std::vector <unique_ptr<IPrintable>> v;
+
+	v.emplace_back(Factory::Create(animal));
+	v.emplace_back(Factory::Create(animal));
+	v.emplace_back(Factory::Create(fish));
+	v.emplace_back(Factory::Create(fish));
+	v.emplace_back(Factory::Create(fish));
+	v.emplace_back(Factory::Create(bird));
+	v.emplace_back(Factory::Create(bird));
+	v.emplace_back(Factory::Create(bird));
+	v.emplace_back(Factory::Create(bird));
+
+	for (int i = 0; i < size(v); i++) {
+		v[i]->Print();
+	}
+
+	v.erase(v.begin(), v.end());
+
+
+}
