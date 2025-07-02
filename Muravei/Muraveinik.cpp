673201e46ed57fa30f843389f7e6a1d6b8630b62
emiliaMetelitsa <klthsrswm@gmail.com>
2025-07-02 14:50:07 +0300

    Create Muraveinik.cpp

---
+++ Muravei/Muraveinik.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <list>
+#include <set>
+
+using namespace std;
+
+struct coords {
+	unsigned int x;
+	unsigned int y;
+
+	bool operator<(const coords& other) const {
+		if (x < other.x) {
+			return true;
+		}
+		else if (x == other.x && y < other.y) {
+			return true;
+		}
+		return false;
+	}
+};
+
+int Sum(int a) {
+
+	int sum = 0;
+	
+	while (a / 10 > 0) {
+		sum += a % 10;
+		a /= 10;
+	}
+	
+	return sum;
+}
+
+bool IsAvaible(int x, int y) {
+	int a = Sum(x) + Sum(y);
+	if (a <= 25) {
+		return true;
+	}
+	return false;
+}
+
+int Finding(int x, int y) {
+
+	set<coords> visited;
+	list<coords> q;
+	
+
+	unsigned int i = x;
+	unsigned int j = y;
+
+	q.push_back({ i,j });
+
+	while (!q.empty()) {
+
+		if (!visited.contains({ i, j })) {
+
+			if ((i < 1000) && (IsAvaible(i + 1, j))) {
+				q.push_back({ i + 1,j });
+			}
+			if ((j < 1000) && (IsAvaible(i, j + 1))) {
+				q.push_back({ i,j + 1 });
+			}
+			if ((i > 0) && (IsAvaible(i - 1, j))) {
+				q.push_back({ i - 1,j });
+			}
+			if ((j > 0) && (IsAvaible(i, j + 1))) {
+				q.push_back({ i,j + 1 });
+			}
+
+		}
+
+		visited.insert(q.front());
+		q.pop_front();
+
+		if (!q.empty()) {
+			coords a = q.front();
+			i = a.x;
+			j = a.y;
+		}
+
+	}
+
+	return visited.size();
+}
+
+int main()
+{
+	int count = Finding(1000, 1000);
+	cout << count << endl;
+	return 0;
+}
